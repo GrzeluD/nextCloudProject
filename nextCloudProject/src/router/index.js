df7ec6c51c7670c6943d636604b7c48e3a27f1ea
Grzegorz Dziedzic <grzelu@localhost.localdomain>
2023-04-18 23:02:50 +0200

    vite config test

--- nextCloudProject/src/router/index.js
+++ nextCloudProject/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Slideshow from "@/views/Slideshow.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_APP_BASE_PATH),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
