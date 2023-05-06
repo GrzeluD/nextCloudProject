1620b6c168bdfdd4da06f75b74bcdb8495e86060
Grzegorz Dziedzic <grzelu@localhost.localdomain>
2023-05-06 20:02:52 +0200

    background figures with random animation

--- nextCloudProject/src/components/BackgroundFigures.vue
+++ nextCloudProject/src/components/BackgroundFigures.vue
@@ -1,12 +1,15 @@
 <template>
   <template v-for="(figure, index) in figures" :key="`figure-${index}`">
     <div :class="`figure figure__${figure.type}`"
+         :data-animation="figure.animation ? drawAnimationType() : ''"
+         :data-time="figure.animation ? drawAnimationTime() : 0"
          :style="`width:${figure.width};
                 height:${figure.height};
                 background:${figure.backgroundColor};
                 border:${figure.border};
                 top:${figure.positionY};
-                left:${figure.positionX};`"></div>
+                left:${figure.positionX};`"
+          ref="figures"></div>
   </template>
 </template>
 
@@ -17,6 +20,35 @@ export default {
     figures: {
       type: Array,
     }
+  },
+  data() {
+    return {
+      animations: ['squareSpinRight', 'squareSpinLeft', 'squareNegativeSpinLeft', 'squareNegativeSpinRight'],
+    }
+  },
+  methods: {
+    drawAnimationType() {
+      return this.animations[Math.floor(Math.random() * this.animations.length)];
+    },
+
+    drawAnimationTime() {
+      return Math.floor(Math.random() * (30 - 20 + 1) + 20);
+    },
+
+    setAnimations(items) {
+      if(items.length > 0) {
+        items.forEach(item => {
+          console.log(item)
+          if (item.dataset.animation !== '') {
+            item.style.animationName = item.dataset.animation;
+            item.style.animationDuration = `${item.dataset.time}s`;
+          };
+        })
+      }
+    }
+  },
+  mounted() {
+    this.setAnimations(this.$refs.figures);
   }
 }
 </script>
@@ -24,5 +56,6 @@ export default {
 <style scoped>
 .figure {
   position: absolute;
+  animation: linear infinite;
 }
 </style>
--- nextCloudProject/src/components/Slides/IntroductionSlide.vue
+++ nextCloudProject/src/components/Slides/IntroductionSlide.vue
@@ -1,8 +1,8 @@
 <template>
-  <section ref="slide" class="slide shrink-0 h-full basis-[100vw] pt-8 relative">
+  <section ref="slide" class="slide flex items-center shrink-0 h-full basis-[100vw] relative">
     <div class="container w-[640px] mx-auto">
       <h2 class="text-3xl mb-8 font-[Sharetech] uppercase">{{ heading }}</h2>
-      <section v-if="creators.length > 0">
+      <section class="relative" v-if="creators.length > 0">
         <div class="creator flex items-center" v-for="(creator, index) in creators" :key="`creator${index}`">
           <figure class="creator__img h-32 w-80 before:z-10 before:z-index-1 before:content-[''] before:opacity-50 relative before:top-0 before:bottom-0 before:left-0 before:right-0 before:absolute" :class="index % 2 !== 0 ? 'order-1' : ''">
             <img class="h-full w-full object-cover grayscale" :src="`${path}/images/${creator.img}.jpeg`" :alt="creator.name">
@@ -12,9 +12,10 @@
             <p>{{ creator.description }}</p>
           </div>
         </div>
+        <BackgroundFigures :figures="creatorsFigures" />
       </section>
     </div>
-    <BackgroundFigures :figures="figures" />
+    <BackgroundFigures :figures="bgFigures" />
   </section>
 </template>
 
@@ -34,15 +35,108 @@ export default {
   data() {
     return {
       path: import.meta.env.PROD ? '/project' : '',
-      figures: [
+      bgFigures: [
         {
           type: 'emptySquare',
-          width: '50px',
-          height: '50px',
-          background: 'transparent',
-          border: '1px solid #fff',
-          positionX: '20px',
-          positionY: '20px'
+          width: '25px',
+          height: '25px',
+          backgroundColor: 'transparent',
+          border: '1px solid #ffffff80',
+          positionX: '30px',
+          positionY: '90%',
+          animation: true,
+        },
+        {
+          type: 'emptySquare',
+          width: '16px',
+          height: '16px',
+          backgroundColor: 'transparent',
+          border: '1px solid #ffffff50',
+          positionX: '85px',
+          positionY: '93%',
+          animation: true,
+        },
+        {
+          type: 'emptySquare',
+          width: '25px',
+          height: '25px',
+          backgroundColor: 'transparent',
+          border: '1px solid #ffffff80',
+          positionX: '97%',
+          positionY: '30px',
+          animation: true,
+        },
+        {
+          type: 'emptySquare',
+          width: '20px',
+          height: '20px',
+          backgroundColor: 'transparent',
+          border: '1px solid #ffffff50',
+          positionX: '92%',
+          positionY: '15px',
+          animation: true,
+        },
+        {
+          type: 'emptySquare',
+          width: '5px',
+          height: '5px',
+          backgroundColor: 'transparent',
+          border: '1px solid #ffffff99',
+          positionX: '89%',
+          positionY: '50px',
+          animation: true,
+        },
+        {
+          type: 'emptySquare',
+          width: '20px',
+          height: '20px',
+          backgroundColor: 'transparent',
+          border: '1px solid #ffffff70',
+          positionX: '84%',
+          positionY: '30px',
+          animation: true,
+        },
+        {
+          type: 'fullSquare',
+          width: '16px',
+          height: '16px',
+          backgroundColor: '#e898ac',
+          border: 'none',
+          positionX: '80%',
+          positionY: '40px',
+          animation: true,
+        },
+        {
+          type: 'line',
+          width: '2px',
+          height: '40px',
+          backgroundColor: 'linear-gradient(#ffffff10, #ffffff90)',
+          border: 'none',
+          positionX: 'calc(80% + 7px)',
+          positionY: '-5px',
+          animation: false,
+        },
+      ],
+      creatorsFigures: [
+        {
+          type: 'fullSquare',
+          width: '20px',
+          height: '20px',
+          backgroundColor: '#ff9973',
+          border: 'none',
+          positionX: '-20px',
+          positionY: '-20px',
+          animation: true,
+        },
+        {
+          type: 'fullSquare',
+          width: '26px',
+          height: '26px',
+          backgroundColor: '#e898ac',
+          border: 'none',
+          positionX: '100%',
+          positionY: 'calc(100% - 26px)',
+          animation: true,
         },
       ],
     }
